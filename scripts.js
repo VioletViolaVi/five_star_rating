e933ff3e1c9234ad794f7a25cc3879b1b0db2d6d
vivian <vivian.v18723@gmail.com>
2022-04-20 22:23:50 +0100

    use js to make first star change colour on click

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  console.log("hello world");
+  // one star function
+  const oneStarRating = () => {
+    const firstBtn = document.getElementById("firstBtn");
+    firstBtn.addEventListener("click", () => {
+      const oneStar = document.getElementById("oneStar");
+      oneStar.style.color = "#fdff00";
+    });
+  };
+  oneStarRating();
 });
