1c39fa8048b99887564f1e8b5febaf2d858aeb3f
vivian <vivian.v18723@gmail.com>
2022-04-20 22:30:56 +0100

    get ids for all star icons

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  // one star function
+document.addEventListener("DOMContentLoaded", () => {
+  // star ids
+  const oneStar = document.getElementById("oneStar");
+  const twoStars = document.getElementById("twoStars");
+  const threeStars = document.getElementById("threeStars");
+  const fourStars = document.getElementById("fourStars");
+  const fiveStars = document.getElementById("fiveStars");
+
+  // one star rating
   const oneStarRating = () => {
     const firstBtn = document.getElementById("firstBtn");
     firstBtn.addEventListener("click", () => {
-      const oneStar = document.getElementById("oneStar");
       oneStar.style.color = "#fdff00";
+      twoStars.style.color = "#dcdcdc";
     });
   };
   oneStarRating();
+
+  // two star rating
+  const twoStarRating = () => {
+    const secondBtn = document.getElementById("secondBtn");
+    secondBtn.addEventListener("click", () => {
+      oneStar.style.color = "#fdff00";
+      twoStars.style.color = "#fdff00";
+    });
+  };
+  twoStarRating();
 });
